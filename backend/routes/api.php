e7b5b1f1368070feb9b30a132da19cfe871310c1
PL Minh Phú <plminhphu@icloud.com>
2025-08-12 18:34:24 +0700

    bỏ hết

--- backend/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\UserController;
-
-Route::prefix('admin')->group(function () {
-    Route::get('/users', [UserController::class, 'index']);
-});
